528021e990a12eba3481405c0f3cf9d07fe906ed
MykhailoGranik <Fcdkbear@gmail.com>
2022-01-26 23:03:38 +0200

    Initial commit

---
+++ src/main/java/GuessColorizer.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class GuessColorizer {
+
+    private String answer;
+
+    public List<LetterColor> colorize(String guess) {
+        return null;
+    }
+
+}
---
+++ src/main/java/LetterColor.java
@@ -0,0 +1,7 @@
+public enum LetterColor {
+
+    GREEN,
+    YELLOW,
+    GREY
+
+}

2bc49c0bca7b5a84ef4a59b4e0a24c65e877d9cf
MykhailoGranik <Fcdkbear@gmail.com>
2022-01-27 22:07:40 +0200

    Updated MiniMaxResult to store buckets

--- src/main/java/wordle/MiniMaxResult.java
+++ src/main/java/wordle/MiniMaxResult.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.Map;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -11,11 +14,13 @@ public class MiniMaxResult {
 
     private String guess;
     private int size;
+    private Map<List<LetterColor>, List<String>> buckets;
 
     public void updateIfNeeded(MiniMaxResult other) {
         if (guess == null || other.size < size) {
             guess = other.guess;
             size = other.size;
+            buckets = other.buckets;
         }
     }
 
--- src/test/java/wordle/MiniMaxResultTest.java
+++ src/test/java/wordle/MiniMaxResultTest.java
@@ -2,6 +2,9 @@ package wordle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MiniMaxResultTest {
@@ -9,28 +12,46 @@ public class MiniMaxResultTest {
     @Test
     public void shouldUpdateIfResultNotSet() {
         MiniMaxResult underTest = new MiniMaxResult();
-        MiniMaxResult other = new MiniMaxResult("a", 2);
+        Map<List<LetterColor>, List<String>> otherBuckets = Map.of(
+                List.of(LetterColor.GREY), List.of("a", "b")
+        );
+        MiniMaxResult other = new MiniMaxResult("a", 2, otherBuckets);
         underTest.updateIfNeeded(other);
         assertEquals(2, underTest.getSize());
         assertEquals("a", underTest.getGuess());
+        assertEquals(otherBuckets, underTest.getBuckets());
     }
 
     @Test
     public void shouldUpdateIfOtherResultIsBetter() {
-        MiniMaxResult underTest = new MiniMaxResult("b", 3);
-        MiniMaxResult other = new MiniMaxResult("a", 2);
+        Map<List<LetterColor>, List<String>> currentBuckets = Map.of(
+                List.of(LetterColor.GREY), List.of("a", "b", "c")
+        );
+        MiniMaxResult underTest = new MiniMaxResult("b", 3, currentBuckets);
+        Map<List<LetterColor>, List<String>> otherBuckets = Map.of(
+                List.of(LetterColor.GREY), List.of("a", "b")
+        );
+        MiniMaxResult other = new MiniMaxResult("a", 2, otherBuckets);
         underTest.updateIfNeeded(other);
         assertEquals(2, underTest.getSize());
         assertEquals("a", underTest.getGuess());
+        assertEquals(otherBuckets, underTest.getBuckets());
     }
 
     @Test
     public void shouldNotUpdateIfOtherResultIsWorse() {
-        MiniMaxResult underTest = new MiniMaxResult("b", 1);
-        MiniMaxResult other = new MiniMaxResult("a", 2);
+        Map<List<LetterColor>, List<String>> currentBuckets = Map.of(
+                List.of(LetterColor.GREY), List.of("a")
+        );
+        MiniMaxResult underTest = new MiniMaxResult("b", 1, currentBuckets);
+        Map<List<LetterColor>, List<String>> otherBuckets = Map.of(
+                List.of(LetterColor.GREY), List.of("a", "b")
+        );
+        MiniMaxResult other = new MiniMaxResult("a", 2, otherBuckets);
         underTest.updateIfNeeded(other);
         assertEquals(1, underTest.getSize());
         assertEquals("b", underTest.getGuess());
+        assertEquals(currentBuckets, underTest.getBuckets());
     }
 
 }
